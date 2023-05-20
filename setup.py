a7c18e64763a86dde603ab348f417e777152bd05
SahilAgarwl <201b226@juetguna.in>
2023-05-20 17:03:22 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Pranab',
+    author_email='prakhar.srivastava040@gmail.com',
+    description='HospitalManagementSystem for college python course'
+)
